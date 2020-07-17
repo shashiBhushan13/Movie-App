65b4839d0a95b1d7735d826f69b280f951dacf1a
Shashi Bhushan <shashibhusha8683@gmail.com>
2020-07-17 07:27:32 +0530

    Added all Feature

--- src/index.js
+++ src/index.js
@@ -1,17 +1,104 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App';
+import rootReducer from './reducers';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+// const logger = function({ dispatch, getState }) {
+//   return function(next) {
+//     return function(action) {
+//       // my middlware
+//       console.log('ACTION', action);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  // my middlware
+  console.log('ACTION', action);
+  next(action);
+};
+
+// const thunk = store => next => action => {
+//   if (typeof action === 'function') {
+//     return action(store.dispatch);
+//   }
+
+//   next(action);
+// };
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+// // console.log(store);
+// console.log('state', store.getState());
+
+// export const StoreContext = createContext();
+
+// console.log('StoreContext', StoreContext);
+
+// class Provider extends React.Component {
+//   render() {
+//     const { store } = this.props;
+//     return (
+//       <StoreContext.Provider value={store}>
+//         {this.props.children}
+//       </StoreContext.Provider>
+//     );
+//   }
+// }
+
+// const connectedComponent = connect(callback)(App);
+// export function connect(callback) {
+//   return function (Component) {
+//     class ConnectedComponent extends React.Component {
+//       constructor(props) {
+//         super(props);
+//         this.unsubscribe = this.props.store.subscribe(() => {
+//           this.forceUpdate();
+//         });
+//       }
+
+//       componentWillUnmount() {
+//         this.unsubscribe();
+//       }
+//       render() {
+//         const { store } = this.props;
+//         const state = store.getState();
+//         const dataToBeSentAsProps = callback(state);
+
+//         return <Component dispatch={store.dispatch} {...dataToBeSentAsProps} />;
+//       }
+//     }
+
+//     class ConnectedComponentWrapper extends React.Component {
+//       render() {
+//         return (
+//           <StoreContext.Consumer>
+//             {(store) => {
+//               return <ConnectedComponent store={store} />;
+//             }}
+//           </StoreContext.Consumer>
+//         );
+//       }
+//     }
+//     return ConnectedComponentWrapper;
+//   };
+// }
+
+// update store by dispatching actions
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: moviesList
+// });
+// console.log('state', store.getState());
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
